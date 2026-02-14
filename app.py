2dff3bf88636864843a94b8a27eebef265b3cd28
Your Name <you@example.com>
2026-02-14 18:38:20 +0200

    Testing my robot with a change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from my first website!"
+    return "Hello from my first website! - 2.0עכיכעכעי"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
